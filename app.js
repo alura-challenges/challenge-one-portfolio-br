f6cbd3060563ebbc300ca79376f079db7e758a40
Carla Rodrigues <rodricah1.8@gmail.com>
2024-05-10 19:40:13 -0400

    media querys

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementsByClassName("sidebar").style.width = "15rem";
+    document.getElementsByClassName("main__document").style.marginLeft = "16rem";
+  }
+  
+  function closeNav() {
+    document.getElementsByClassName("sidebar").style.width = "0";
+    document.getElementsByClassName("main__document").style.marginLeft= "0";
+  }
